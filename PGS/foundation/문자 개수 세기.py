31036c194d9535acca1f69990ed28d32867de508
JYJ <abruoah@naver.com>
2024-03-10 18:12:41 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/181902

---
+++ PGS/foundation/문자 개수 세기.py
@@ -0,0 +1,10 @@
+def solution(my_string):
+    my_string = list(my_string)
+    alphabet_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+                     'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                     'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    answer = [my_string.count(x) for x in alphabet_list]
+
+    return answer

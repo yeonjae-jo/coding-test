bf959d3391e0677df6efc24c95ab3fb84a50a0e6
JYJ <abruoah@naver.com>
2024-03-16 23:48:38 +0900

    약수의 덧셈

---
+++ javaScript/programmers/level_1/약수의 개수와 덧셈.js
@@ -0,0 +1,12 @@
+function solution(l, r) {
+    let arr = [], sum = 0;
+        for(i=l; i<=r; i++){
+            for(j=1; j<=i; j++){
+                if(i%j === 0) 
+                    arr.push(j);
+            }
+            sum = arr.length % 2 == 0? sum +=i : sum -= i;
+            arr = []
+        }
+        return sum  
+    }

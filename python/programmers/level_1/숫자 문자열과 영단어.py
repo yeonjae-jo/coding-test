2ba8916d1aeee9acc4515a379c0483f8d82a9669
JYJ <abruoah@naver.com>
2024-05-12 19:45:47 +0900

    2024.05.12

---
+++ python/programmers/level_1/숫자 문자열과 영단어.py
@@ -0,0 +1,7 @@
+def solution(s):
+    word = {'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4',
+            'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
+
+    for key in word:
+        s = s.replace(key, word[key])
+    return int(s)
